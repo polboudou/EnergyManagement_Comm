32822f9d59943c16ee185d0c43ce3800fb50190b
polboudou <polboudou@github.com>
2019-11-18 18:09:36 +0100

    added heat losses to boiler temperature model

--- python_scenarios/scenario1.py
+++ python_scenarios/scenario1.py
@@ -8,12 +8,14 @@ POWER = 1                # code clarity variable
 TEMP = 0                 # code clarity variable
 Dt = 1                   # control period (min)
 
-B_TMIN = 20             # Min boiler temperature (degrees celcius)
-B_TMAX = 22             # Max boiler temperature (degree celcius)
+B_TMIN = 40             # Min boiler temperature (degrees celcius)
+B_TMAX = 42             # Max boiler temperature (degree celcius)
 B_PMAX = -7.6           # Max boiler power (kW)
 C_W = 0.00113           # specific heat of water  (kWh/(kg*K))
 B_VOLUME = 300          # boiler's volume (l)
 C_B = B_VOLUME * C_W    # boiler thermal capacity (kWh/K)
+T_AMB = 22              # ambient temperature (degrees celsius)
+t_HL = 400              # time constant for heat loss (min)
 
 
 
@@ -21,7 +23,7 @@ def algo_scenario1(boiler_states, p_x):
 
     u_B = {1: 0, 2: 0}
     p_x = p_x + boiler_states[1][POWER] + boiler_states[2][POWER]
-
+    print("px en considérant les boilers:", p_x)
     boiler_states_sorted = sorted(boiler_states.items(), key=operator.itemgetter(1))
 
     for (boiler, state) in boiler_states_sorted:
@@ -36,12 +38,13 @@ def algo_scenario1(boiler_states, p_x):
     print('u_B = ', u_B)
     return u_B
 
+#START:
 
 # Initialization:
 boiler_states = []
 boiler_control = []
-T_B1 = 21
-T_B2 = 19.5
+T_B1 = 41
+T_B2 = 39.5
 p_B1 = 0
 p_B2 = 0
 init_boiler_states = {1: [T_B1, p_B1], 2: [T_B2, p_B2]}
@@ -49,7 +52,7 @@ init_boiler_states = {1: [T_B1, p_B1], 2: [T_B2, p_B2]}
 boiler_states.append(init_boiler_states)
 
 
-# In roder to test the algorithm, we simulate the power excess (P_PV - P_nc)
+# In order to test the algorithm, we simulate the power excess (P_PV - P_nc)
 excess = pd.read_excel('data_input/Energie - 00003 - Pache.xlsx', index_col=[0], usecols=[0, 1])
 excess['P_PV - P_nc (kW)'] = excess['Flux energie au point d\'injection (kWh)'] * -6  # Convert the energy (kWh) to power (kW) and power convention (buy positive and sell negative)
 del excess['Flux energie au point d\'injection (kWh)']  # we do not need the energy column anymore
@@ -61,11 +64,15 @@ for h, p_x in enumerate(excess['P_PV - P_nc (kW)']):
     boiler_control.append(algo_scenario1(boiler_states[h], p_x))
 
     # In order to test the algorithm, I set boilers power close to the desired action u
-    p_B1 = boiler_control[h][1] + 0.1 * boiler_control[h][1]
-    p_B2 = boiler_control[h][2] + 0.1 * boiler_control[h][2]
-    T_B1 = boiler_states[h][1][TEMP] - p_B1 * (Dt/60) / C_B
-    T_B2 = boiler_states[h][2][TEMP] - p_B2 * (Dt/60) / C_B
-    print('T_B1:', T_B1, 'and T_B2:', T_B2)
+    p_B1 = boiler_control[h][1] - 0.1 * boiler_control[h][1]
+    p_B2 = boiler_control[h][2] - 0.1 * boiler_control[h][2]
+
+    # temperature evolves as a function of boiler's input power according to the simple model below
+    T_B1 = boiler_states[h][1][TEMP] - p_B1 * (Dt/60) / C_B - (boiler_states[h][1][TEMP] - T_AMB) * (Dt/t_HL)
+    T_B2 = boiler_states[h][2][TEMP] - p_B2 * (Dt/60) / C_B - (boiler_states[h][1][TEMP] - T_AMB) * (Dt/t_HL)
+
+    print('T_B1, T_B2:', T_B1, T_B2, "p_B1, p_B2:", p_B1, p_B2)
     boiler_states.append({1: [T_B1, p_B1], 2: [T_B2, p_B2]})
+    #print(boiler_states)
 
-
+# END
